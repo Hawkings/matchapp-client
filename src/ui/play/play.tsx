16cb1fbeb0cb0e63d32ee560dc0ee3b25d7883e1
Hawkings <daniel.marin.94@gmail.com>
2023-02-22 22:36:56 +0100

    improve the wording of the question

--- src/ui/play/play.tsx
+++ src/ui/play/play.tsx
@@ -1,8 +1,19 @@
 import { Button, Stack, Typography } from "@mui/material";
 import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { useSession } from "../../lib/session-context";
 import { QuestionType } from "../../__generated__/graphql";
+import styled from "styled-components";
+
+const GreenText = styled.span`
+	color: #9ccc65;
+	font-weight: bold;
+`;
+
+const RedText = styled.span`
+	color: #ef5350;
+	font-weight: bold;
+`;
 
 export default function Play() {
 	const { t } = useTranslation();
@@ -13,18 +24,26 @@ export default function Play() {
 		session.submitAnswer(index);
 		setVoted(true);
 	}
-	const text =
-		session.group?.question?.type === QuestionType.Agree ? t("game.agree") : t("game.disagree");
 	if (voted) {
 		return (
 			<Stack>
-				<Typography variant="h2">{t("game.wait")}</Typography>
+				<Typography variant="h2">{t("play.wait")}</Typography>
 			</Stack>
 		);
 	} else {
 		return (
 			<Stack>
-				<Typography variant="h2">{text}</Typography>
+				<Typography variant="h2">
+					{session.group?.question?.type === QuestionType.Agree ? (
+						<Trans i18nKey="play.questionAgree">
+							Pick the option you think will be the <GreenText>most</GreenText> voted
+						</Trans>
+					) : (
+						<Trans i18nKey="play.questionDisagree">
+							Pick the option you think will be the <RedText>least</RedText> voted
+						</Trans>
+					)}
+				</Typography>
 				<Stack direction="row">
 					{question.answers.map((answer, i) => (
 						<Button key={answer.text} variant="outlined" onClick={() => select(i)}>
